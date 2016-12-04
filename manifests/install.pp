0d2e53595fa5722c307b2238b6bb5f8c31228442
Patrik Majer <patrik.majer@atlas.cz>
2016-12-04 19:02:14 +0100

    fix puppet lint warnings

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class sugarcrmstack_ng::install {
 
   if($::sugarcrmstack_ng::manage_utils_packages){
     package { $::sugarcrmstack_ng::utils_packages:
-      ensure => installed,
+      ensure => "installed",
 #    require => [ Package["epel-repo"],
 #                Ini_setting['remi repo exclude packages'],
 #                Ini_setting['centos base repo exclude packages 2'],
