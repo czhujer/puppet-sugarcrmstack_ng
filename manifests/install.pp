02e744c348990fb5ac71bbfbf59af79054626661
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-07-29 15:47:15 +0200

    add install epel repo

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,14 +4,24 @@
 #
 class sugarcrmstack_ng::install {
 
+  # install epel repo
+  if ( !defined(Yumrepo['epel']) and !defined(Package['epel-release'])){
+    package { 'epel-release':
+      ensure => 'installed',
+    }
+    $require_utils_packages = Package['epel-release']
+  }
+  else{
+    $require_utils_packages = Yumrepo['epel']
+  }
+
   if($::sugarcrmstack_ng::manage_utils_packages){
     package { $::sugarcrmstack_ng::utils_packages:
       ensure => 'installed',
-#    require => [ Package["epel-repo"],
+      require => $require_utils_packages,
 #                Ini_setting['remi repo exclude packages'],
 #                Ini_setting['centos base repo exclude packages 2'],
 #                Ini_setting['centos base repo exclude packages'],
-#            ]
     }
   }
 }
