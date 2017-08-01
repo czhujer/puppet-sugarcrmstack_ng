0cd9c3e4d53ca2f53d9b732382899be432ba676c
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-08-01 21:28:14 +0200

    add defs for remove old mysql/pxc packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -72,4 +72,38 @@ class sugarcrmstack_ng::install {
 #                Ini_setting['centos base repo exclude packages'],
     }
   }
+
+  # uninstall old mysql/percona packages
+  if ($::sugarcrmstack_ng::mysql_server_enable){
+
+    if ($::operatingsystemmajrelease in ['7'] and $::sugarcrmstack_ng::mysql_server_use_pxc == true) {
+      $mysql_server_packages_old = ["mariadb", "mariadb-server", "mysql-community-server", "mysql-community-client" ]
+    }
+    elsif ($::operatingsystemmajrelease in ['7'] and $::sugarcrmstack_ng::mysql_server_use_pxc == false) {
+      $mysql_server_packages_old = ["mariadb", "mariadb-server",
+                                    "Percona-XtraDB-Cluster-shared-56", "Percona-XtraDB-Cluster-client-56",
+                                    "Percona-XtraDB-Cluster-shared-57", "Percona-XtraDB-Cluster-client-57",
+                                   ]
+    }
+    elsif ($::operatingsystemmajrelease in ['6'] and $::sugarcrmstack_ng::mysql_server_use_pxc == true and $::sugarcrmstack_ng::sugar_version == '7.5'){
+      $mysql_server_packages_old = ["mysql55", "mysql55-libs", "mysql55-server", "mysql-community-server", "mysql-community-client" ]
+    }
+    elsif ($::operatingsystemmajrelease in ['6'] and $::sugarcrmstack_ng::mysql_server_use_pxc == false and $::sugarcrmstack_ng::sugar_version == '7.5'){
+      $mysql_server_packages_old = ["mysql55", "mysql55-libs", "mysql55-server"]
+    }
+    elsif ($::operatingsystemmajrelease in ['6'] and $::sugarcrmstack_ng::mysql_server_use_pxc == true and $::sugarcrmstack_ng::sugar_version == '7.9'){
+      $mysql_server_packages_old = ["mysql55", "mysql55-libs", "mysql55-server", "mysql-community-server", "mysql-community-client" ]
+    }
+    elsif ($::operatingsystemmajrelease in ['6'] and $::sugarcrmstack_ng::mysql_server_use_pxc == false and $::sugarcrmstack_ng::sugar_version == '7.9'){
+      $mysql_server_packages_old = ["mysql55", "mysql55-libs", "mysql55-server"]
+    }
+    else{
+    }
+
+    package { $mysql_server_packages_old:
+      ensure   => 'absent',
+      provider => 'yum',
+    }
+  }
+
 }
