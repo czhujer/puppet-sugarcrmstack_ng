b7109a2ffb304fffce32a4d51ded594cba168afe
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-12-06 15:59:00 +0100

    add testsuite for elasticsearch_server

---
+++ spec/classes/main_w_es_spec.rb
@@ -0,0 +1,96 @@
+require 'spec_helper'
+
+describe 'sugarcrmstack_ng' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        #fixes for composer and mysql (root_home)
+        let(:facts) { facts.merge( { 'composer_home' => '~', 'execs' => {}, 'root_home' => '/root' } ) }
+
+        context "sugarcrmstack_ng class with elasticsearch_server_enable" do
+          # switch param
+          let(:params) { {'elasticsearch_server_enable' => true} }
+
+          # check compile
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_class('sugarcrmstack_ng') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::params') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::install') }
+#          it { is_expected.to contain_class('sugarcrmstack_ng::install').that_comes_before('sugarcrmstack_ng::config') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::config') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::service') }
+#          it { is_expected.to contain_class('sugarcrmstack_ng::service').that_subscribes_to('sugarcrmstack_ng::config') }
+
+          it { should contain_class('sugarcrmstack_ng::apache_php') }
+          it { should contain_class('sugarcrmstack_ng::elasticsearch_server') }
+
+          # generic part
+          ['apachetop', 'bind-utils', 'htop', 'iftop',
+           'iotop', 'iptraf', 'irqbalance', 'links', 'lsof', 'lsscsi', 'lynx',
+           'nano', 'numad', 'policycoreutils-python', 'sysstat', 'telnet', 'traceroute',
+           'unzip', 'vim-enhanced', 'vim-minimal', 'wget', 'yum-cron', 'yum-utils', 'zip',
+           ].each do |x| it {
+             is_expected.to contain_package(x)
+               .with(ensure: "installed")
+             }
+          end
+
+          # only CentOS 6 packages
+          case facts[:osfamily]
+          when 'RedHat'
+            case facts[:operatingsystemmajrelease]
+              when '6'
+                it { should contain_package("hal").with(ensure: "installed") }
+                it { should contain_package("cpuspeed").with(ensure: "installed") }
+              else
+                it { should_not contain_package("hal").with(ensure: "installed") }
+                it { should_not contain_package("cpuspeed").with(ensure: "installed") }
+              end
+          end
+
+          # apache+php part
+          it { should contain_class("apache::params") }
+          it { should contain_package("httpd") }
+
+          # elasticsearch_server part
+          it { should contain_class('java') }
+          it { should contain_class('elasticsearch::config').that_requires('Class[java]') }
+          it { should contain_class('elasticsearch::repo') }
+          it { should contain_class('elasticsearch') }
+          it { should contain_class('elasticsearch::package') }
+          it { should contain_class('elasticsearch::config').that_requires('Class[elasticsearch::package]') }
+
+          # Base directories
+          it { should contain_file('/etc/elasticsearch') }
+          it { should contain_file('/usr/share/elasticsearch') }
+
+          # Base package
+          it { should contain_package('elasticsearch') }
+
+          # Repo
+          it { should contain_yumrepo('elasticsearch') }
+
+        end
+
+      end
+    end
+  end
+
+  context 'unsupported operating system' do
+    describe 'sugarcrmstack_ng class without any parameters on Solaris/Nexenta' do
+      let(:facts) do
+        {
+          :osfamily        => 'Solaris',
+          :operatingsystem => 'Nexenta',
+        }
+      end
+
+      it { expect { is_expected.to contain_package('sugarcrmstack_ng') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
+    end
+  end
+end
