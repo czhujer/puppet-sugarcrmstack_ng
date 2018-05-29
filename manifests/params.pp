72d63cee22d8f1bf8becd664158320c218594691
Valy <jakub.valenta@sugarfactory.cz>
2018-05-29 16:36:44 +0200

    added proxy pass match default for phpMyAdmin

--- manifests/params.pp
+++ manifests/params.pp
@@ -38,8 +38,22 @@ class sugarcrmstack_ng::params {
         $apache_php_php_pkg_build = '1'
 
         $apache_php_proxy_pass_match = []
-        $apache_php_proxy_pass_match_default = [
-           { 'path' => '^/(.*\.php(/.*)?)$', 'url'  => 'fcgi://127.0.0.1:9001/var/www/html/sugarcrm/$1' },
+        $apache_php_proxy_pass_match_default = [ {
+             'path' => '^/phpmyadmin/(.*\.php)$',
+             'url'  => 'fcgi://127.0.0.1:9002/usr/share/phpMyAdmin/$1',
+          },{
+             'path' => '/phpmyadmin(.*/)$',
+             'url'  => 'fcgi://127.0.0.1:9002/usr/share/phpMyAdmin$1index.php'
+          },{
+             'path' => '^/phpMyAdmin/(.*\.php)$',
+             'url'  => 'fcgi://127.0.0.1:9002/usr/share/phpmyadmin/$1'
+          },{
+             'path' => '^/phpMyAdmin(.*/)$',
+             'url'  => 'fcgi://127.0.0.1:9002/usr/share/phpMyAdmin$1index.php'
+          },{
+             'path' => '^/(.*\.php(/.*)?)$',
+             'url'  => 'fcgi://127.0.0.1:9001/var/www/html/sugarcrm/$1'
+          }
         ]
 
         $memcached_php_module_name = 'php-pecl-memcache'
